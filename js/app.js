e03c5943f00e7cde701154c8d5b4303db580e6e6
Tushar <tushar@wohlig.com>
2015-05-19 12:56:54 +0530

    Some Improvements

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-        }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
+            StatusBar.overlaysWebView(true);
             StatusBar.styleLightContent();
         }
     });
@@ -24,9 +13,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
     $ionicConfigProvider.tabs.position('bottom');
     $ionicConfigProvider.scrolling.jsScrolling(false);
+
     $stateProvider
 
-    // setup an abstract state for the tabs directive
     .state('tab', {
         url: "/tab",
         abstract: true,
@@ -53,7 +42,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
                 controller: 'RegisterCtrl'
             }
         })
-    // Each tab has its own nav history stack:
 
     .state('tab.home', {
         url: '/home',
@@ -113,8 +101,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
     });
 
-
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tab/home');
-
 });
